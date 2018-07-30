171fa53cdce5dffbd74a7995de424b830bb55885
jordynmcginnis <jordynm4@gmail.com>
2018-07-30 14:14:27 -0700

    edited changedHandle

--- src/components/search.js
+++ src/components/search.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import GoMarkGithub from 'react-icons/lib/go/mark-github';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleKey = this.handleKey.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleKey ({target}) {
+    let value = target.value;
+    this.setState(({value}));
+  }
+  handleSubmit () {
+    this.props.handleClick(this.state.value)
+  }
   render() {
     return (
       <div className="search">
         <GoMarkGithub className='github-icon'/>
-        <input className='search-input' placeholder='Enter username... like jordynMcginnis' onKeyPress={(e) => e.key === 'Enter' ? this.props.handleClick(e) : null}/>
+        <input className='search-input' placeholder='Enter username... like jordynMcginnis' onChange={this.handleKey} onKeyPress={(e) => e.key === 'Enter' ? this.handleSubmit() : null}/>
+        <button onClick={this.handleSubmit}>Search</button>
       </div>
     );
   }
