dc6d950eaa6d810cd288aa8a87ab6db3788812c2
jordynmcginnis <jordynm4@gmail.com>
2018-07-30 14:23:41 -0700

    deconstructed

--- src/components/Fan.js
+++ src/components/Fan.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 
 class Fan extends Component {
-  render({props}) {
+  render() {
+    let props = this.props;
     let main = props.person;
-    const result = props.events.reduce((result, {item}) => {
+    const result = props.events.reduce((result, {actor, repo}) => {
       if(!result[actor.login] && repo.name.indexOf(main) > -1){
         result[actor.login] = 1;
       } else if(repo.name.indexOf(main) > -1){
@@ -19,13 +20,13 @@ class Fan extends Component {
         name = key;
       }
     };
-    let personEvent = props.events.filter(({item}) => actor.login === name);
+    let personEvent = props.events.filter(({actor}) => actor.login === name);
     return (
       <div className='Contributions'>
         <p className='fan-name'>  {name === '' ? 'No Stalker' : name} </p>
         <div className='fan-info'> <span className='stats'> Github Stalker - </span>  Results based on recent activity from users associated with {props.person}'s account.</div>
         <div className='fan-results'>
-          {personEvent.map(({item}) => <div className='f-result' key={repo.name}> <span>{type}:</span>{repo.name}<span>{created_at.slice(5,7)}/{created_at.slice(8,10)}/{created_at.slice(0,4)}</span></div>)}
+          {personEvent.map(({repo, created_at, type}) => <div className='f-result' key={repo.name}> <span>{type}:</span>{repo.name}<span>{created_at.slice(5,7)}/{created_at.slice(8,10)}/{created_at.slice(0,4)}</span></div>)}
         </div>
       </div>
     );
