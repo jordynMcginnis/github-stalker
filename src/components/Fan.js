a2d3d8bc2089cfc6f416a84e46b9603a4587dcf0
jordynmcginnis <jordynm4@gmail.com>
2018-07-30 13:44:08 -0700

    es6

--- src/components/Fan.js
+++ src/components/Fan.js
@@ -1,46 +1,34 @@
 import React, { Component } from 'react';
 
 class Fan extends Component {
-
   render() {
-    var main = this.props.person;
-    var result = this.props.events.reduce(function(result, item){
-          if(!result[item.actor.login] && item.repo.name.indexOf(main) > -1){
-            result[item.actor.login] = 1;
-          } else if(item.repo.name.indexOf(main) > -1){
-            result[item.actor.login] = result[item.actor.login] + 1;
-          }
-          return result;
-        }, {});
-    var name = '';
-    var count = 0;
-    for(var key in result){
+    let main = this.props.person;
+    const result = this.props.events.reduce((result, item) => {
+      if(!result[item.actor.login] && item.repo.name.indexOf(main) > -1){
+        result[item.actor.login] = 1;
+      } else if(item.repo.name.indexOf(main) > -1){
+        result[item.actor.login] = result[item.actor.login] + 1;
+      }
+      return result;
+    }, {});
+    let name = '';
+    let count = 0;
+    for(let key in result){
       if(result[key] > count){
         count = result[key];
         name = key;
       }
     };
-    var personEvent = this.props.events.filter(function(item){
-      return item.actor.login === name;
-    });
-
-    // var photo = this.props.list.filter(function(item){
-    //   console.log(this.props.list)
-    //   return item.login === name;
-    // })[0];
+    let personEvent = this.props.events.filter((item) => item.actor.login === name);
     return (
       <div className='Contributions'>
         <p className='fan-name'>  {name === '' ? 'No Stalker' : name} </p>
         <div className='fan-info'> <span className='stats'> Github Stalker - </span>  Results based on recent activity from users associated with {this.props.person}'s account.</div>
         <div className='fan-results'>
-
-          {personEvent.map(function(item){
-            return <div className='f-result'> <span> {item.type}: </span>  {item.repo.name}<span>  {item.created_at.slice(5,7)}/{item.created_at.slice(8,10)}/{item.created_at.slice(0,4)}</span> </div>
-          })}
+          {personEvent.map((item) => <div className='f-result' key={item.repo.name}> <span>{item.type}:</span>{item.repo.name}<span>{item.created_at.slice(5,7)}/{item.created_at.slice(8,10)}/{item.created_at.slice(0,4)}</span></div>)}
         </div>
       </div>
     );
   }
 }
-
 export default Fan;
