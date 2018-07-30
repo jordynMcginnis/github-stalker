aafb8cb7b509517bf75899a273e09a6137bd8794
jordynmcginnis <jordynm4@gmail.com>
2018-07-30 14:09:11 -0700

    deconstructed args

--- src/components/Fan.js
+++ src/components/Fan.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Fan extends Component {
-  render() {
-    let main = this.props.person;
-    const result = this.props.events.reduce((result, item) => {
-      if(!result[item.actor.login] && item.repo.name.indexOf(main) > -1){
-        result[item.actor.login] = 1;
-      } else if(item.repo.name.indexOf(main) > -1){
-        result[item.actor.login] = result[item.actor.login] + 1;
+  render({props}) {
+    let main = props.person;
+    const result = props.events.reduce((result, {item}) => {
+      if(!result[actor.login] && repo.name.indexOf(main) > -1){
+        result[actor.login] = 1;
+      } else if(repo.name.indexOf(main) > -1){
+        result[actor.login] = result[actor.login] + 1;
       }
       return result;
     }, {});
@@ -19,13 +19,13 @@ class Fan extends Component {
         name = key;
       }
     };
-    let personEvent = this.props.events.filter((item) => item.actor.login === name);
+    let personEvent = props.events.filter(({item}) => actor.login === name);
     return (
       <div className='Contributions'>
         <p className='fan-name'>  {name === '' ? 'No Stalker' : name} </p>
-        <div className='fan-info'> <span className='stats'> Github Stalker - </span>  Results based on recent activity from users associated with {this.props.person}'s account.</div>
+        <div className='fan-info'> <span className='stats'> Github Stalker - </span>  Results based on recent activity from users associated with {props.person}'s account.</div>
         <div className='fan-results'>
-          {personEvent.map((item) => <div className='f-result' key={item.repo.name}> <span>{item.type}:</span>{item.repo.name}<span>{item.created_at.slice(5,7)}/{item.created_at.slice(8,10)}/{item.created_at.slice(0,4)}</span></div>)}
+          {personEvent.map(({item}) => <div className='f-result' key={repo.name}> <span>{type}:</span>{repo.name}<span>{created_at.slice(5,7)}/{created_at.slice(8,10)}/{created_at.slice(0,4)}</span></div>)}
         </div>
       </div>
     );
