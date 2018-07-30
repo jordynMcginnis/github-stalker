937cac1434cc8330d7b63c1ddf1209bb1e230402
jordynmcginnis <jordynm4@gmail.com>
2018-07-30 13:47:44 -0700

    cleaned up

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,31 +2,27 @@ import React, { Component } from 'react';
 
 class Events extends Component {
   render() {
-      var order = this.props.events.sort(function(a, b){
-        return a.created_at - b.created_at
-      });
-      var results = Object.keys(this.props.events.reduce(function(result, item){
-          if(!result[item.type]){
-            result[item.type] = 1;
-          } else {
-            result[item.type] = result[item.type] + 1;
-          }
-          return result;
-        }, {}));
-        var other = Object.values(this.props.events.reduce(function(result, item){
-          if(!result[item.type]){
-            result[item.type] = 1;
-          } else {
-            result[item.type] = result[item.type] + 1;
-          }
-          return result;
-        }, {}));
+    let order = this.props.events.sort((a, b) => a.created_at - b.created_at);
+    let results = Object.keys(this.props.events.reduce((result, item) => {
+      if(!result[item.type]){
+        result[item.type] = 1;
+      } else {
+        result[item.type] = result[item.type] + 1;
+      }
+      return result;
+    }, {}));
+    let other = Object.values(this.props.events.reduce((result, item) => {
+      if(!result[item.type]){
+        result[item.type] = 1;
+      } else {
+        result[item.type] = result[item.type] + 1;
+      }
+      return result;
+    }, {}));
     return (
       <div className='Events'>
         <p> Events from Users from  {order[order.length - 1].created_at.slice(5,7)}/{order[order.length - 1].created_at.slice(8,10)}/{order[order.length - 1].created_at.slice(0,4)} to  {order[0].created_at.slice(5,7)}/{order[0].created_at.slice(8,10)}/{order[0].created_at.slice(0,4)}</p>
-        {results.map(function(item, index){
-          return <div className= 'f-result'> {item} : {other[index]} </div>
-        })}
+        {results.map((item, index) => <div className= 'f-result' key={item}> {item} : {other[index]} </div> )}
       </div>
     );
   }
