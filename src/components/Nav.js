f8929b90c1c377d14ead95b59ffc50e0a99b9ea4
jordynmcginnis <jordynm4@gmail.com>
2019-01-07 15:00:58 -0700

    removed space

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Contributions from './Contributions.js';
 import Followers from './Followers.js';
 import Fan from './Fan.js';
 import Events from './Events.js';
@@ -16,7 +15,6 @@ class Nav extends Component {
   handleClick (e, selection) {
     this.setState(({render: selection}))
   }
-     // <div onClick = {(e) => this.handleClick(e, 'Contributions')}>Contributions</div>
   render() {
     return (
       <div className="Nav">
@@ -27,7 +25,6 @@ class Nav extends Component {
           <div onClick = {(e) => this.handleClick(e, 'Issues')}> Issues</div>
         </div>
         <div className='results'>
-          {this.state.render === 'Contributions' ? <Contributions contributions={this.props.contributions}/> : null}
           {this.state.render === 'Followers' ? <Followers contributions={this.props.followers} list={this.props.list} person={this.props.person}/> : null}
           {this.state.render === 'Events' ? <Events events={this.props.events}/> : null}
           {this.state.render === 'Fan' ? <Fan events={this.props.events} person={this.props.person} list={this.props.list}/> : null}
