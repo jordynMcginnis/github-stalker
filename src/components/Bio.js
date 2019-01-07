0541a8868789e01ec9936e2dd0b2f2d9c1af1298
jordynmcginnis <jordynm4@gmail.com>
2019-01-07 16:26:32 -0700

    cleaned up

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,11 +7,13 @@ class Bio extends Component {
       <div className="Bio">
         <div className='bio-photo' style={{backgroundImage: "url(" + Background + ")"}}></div>
         <h1>{this.props.name}</h1>
-        <h3>{this.props.userName}</h3>
-        <p className='sum'> {this.props.summary} </p>
-        <p>Joined Github: {this.props.joined.slice(5,7)}/{this.props.joined.slice(8,10)}/{this.props.joined.slice(0,4)}</p>
-        <p>Location: {this.props.location}</p>
-        <p>Public Repos: {this.props.repos}</p>
+        <div className='bio-info'>
+          <h3>{this.props.userName}</h3>
+          <p className='sum'> {this.props.summary} </p>
+          <p>Joined Github: {this.props.joined.slice(5,7)}/{this.props.joined.slice(8,10)}/{this.props.joined.slice(0,4)}</p>
+          <p>Location: {this.props.location}</p>
+          <p>Public Repos: {this.props.repos}</p>
+        </div>
       </div>
     );
   }
