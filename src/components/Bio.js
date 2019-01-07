dba91e0c64da78ce84a98e3106142731e2b96d01
jordynmcginnis <jordynm4@gmail.com>
2019-01-07 14:57:46 -0700

    removed line space

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,10 +6,9 @@ class Bio extends Component {
     return (
       <div className="Bio">
         <div className='bio-photo' style={{backgroundImage: "url(" + Background + ")"}}></div>
-        {console.log('bio:', Background)}
         <h1>{this.props.name}</h1>
         <h3>{this.props.userName}</h3>
-        <p>{this.props.summary}</p>
+        <p className='sum'> {this.props.summary} </p>
         <p>Joined Github: {this.props.joined.slice(5,7)}/{this.props.joined.slice(8,10)}/{this.props.joined.slice(0,4)}</p>
         <p>Location: {this.props.location}</p>
         <p>Public Repos: {this.props.repos}</p>
