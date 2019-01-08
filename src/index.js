73fc6cff30d77b2e885d1f0fa911f167b9d42b95
jordynmcginnis <jordynm4@gmail.com>
2019-01-08 16:33:03 -0700

    added line space

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './Home';
+
 ReactDOM.render(<Home/>, document.getElementById('root'));
 registerServiceWorker();
