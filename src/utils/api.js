ceaf950254eaf880c514791079db3ae669caea8f
jordynmcginnis <jordynm4@gmail.com>
2019-01-08 17:05:53 -0700

    removed unused func

--- src/utils/api.js
+++ src/utils/api.js
@@ -22,27 +22,6 @@ module.exports = {
       return user.data
     });
   },
-  // getFollowers: (username) => {
-    // var count = -1;
-    // var top5 = [];
-    // return axios.get('https://api.github.com/users/'+ username + '/followers?per_page=100'+ '&' + params, header )
-    // .then(function(users){
-    //   count = users.data.length;
-    //   for(var i = 0; i < users.data.length; i++){
-    //     return axios.get('https://api.github.com/users/' + users.data[i].login + '?' + params)
-    //      .then(function(user){
-    //       top5.push(user.data);
-
-    //      if(top5.length === count){
-    //       return top5;
-    //      }
-    //     });
-    //   }
-    //   while(top5.length === 0){
-    //     return 'hi'
-    //   }
-    // });
-  // },
   fetchFollowers: (username, num) => {
     return axios.get('https://api.github.com/users/' + username + '/followers?per_page=100&page='+num +'&' + params, header )
     .then(function(user){
