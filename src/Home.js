47a430632c594fe3c380f3c252a9df5cba91eaee
jordynmcginnis <jordynm4@gmail.com>
2019-01-09 10:02:09 -0700

    removed line space

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from './App.js';
 import Search from './components/search.js';
 
-
 class Home extends Component {
   render() {
     return (
