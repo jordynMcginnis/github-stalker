3d049bc46273d19d3d0f40942e7d69ccd32f7908
jordynmcginnis <jordynm4@gmail.com>
2019-01-08 17:47:32 -0700

    descructured props

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,16 +3,17 @@ import React, { Component } from 'react';
 class Bio extends Component {
   render() {
     var Background = this.props.photo;
+    const { name, userName, summary, joined, location, repos } = this.props;
     return (
       <div className="Bio">
         <div className='bio-photo' style={{backgroundImage: "url(" + Background + ")"}}></div>
-        <span><h2 className='t-name'>{this.props.name}</h2></span>
+        <span><h2 className='t-name'>{name}</h2></span>
         <div className='bio-info'>
-          <h3>@{this.props.userName}</h3>
-          <p className='sum'> {this.props.summary} </p>
-          <p>Joined Github: {this.props.joined.slice(5,7)}/{this.props.joined.slice(8,10)}/{this.props.joined.slice(0,4)}</p>
-          <p>Location: {this.props.location}</p>
-          <p>Public Repos: {this.props.repos}</p>
+          <h3>@{userName}</h3>
+          <p className='sum'> {summary} </p>
+          <p>Joined Github: {joined.slice(5,7)}/{joined.slice(8,10)}/{joined.slice(0,4)}</p>
+          <p>Location: {location}</p>
+          <p>Public Repos: {repos}</p>
         </div>
       </div>
     );
